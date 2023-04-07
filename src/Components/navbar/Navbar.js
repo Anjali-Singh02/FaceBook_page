4e9b2f466ae6387fa24a01850ad8077ce8849b44
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-07 15:59:42 +0530

    added icons

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from "react";
 import "./navbar.css";
-
+import {
+  SearchOutlined,
+  UserOutlined,
+  MessageOutlined,
+  BellFilled
+} from "@ant-design/icons";
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navContainer">
+      <div className="navbarLeft">
+        <span className="logo">Facebook</span>
+      </div>
+      <div className="navbarCenter">
+        <div className="searchBar"></div>
+        <SearchOutlined />
+        <input type="text" placeholder="Search..." className="searchInput" />
+      </div>
+      <div className="navbarRight">
+        <div className="navbarLinks">
+          <div className="navLink">Homepage</div>
+          <div className="navLink">Timeline</div>
+        </div>
+        <div className="navIcons">
+          <div className="navIconItem">
+            <UserOutlined />
+            <span className="navIconBadge">1</span>
+          </div>
+          <div className="navIconItem">
+          <MessageOutlined />
+            <span className="navIconBadge">2</span>
+          </div>
+          <div className="navIconItem">
+          <BellFilled />
+            <span className="navIconBadge">2</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
